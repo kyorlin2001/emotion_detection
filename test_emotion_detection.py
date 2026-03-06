4b871ff00c747be22d5942fcd5390816eb854df0
kyorlin2001 <kyorlin2001@gmail.com>
2026-03-05 19:30:21 -0500

    updated unit tests

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -6,16 +6,18 @@ from EmotionDetection import emotion_detection
 class TestEmotionDetection(unittest.TestCase):
     def test_emotion_detection(self):
         result1=emotion_detection.emotion_detection("I am glad this happened")
-        self.assertEqual(result1['emotion'],'joy')
+        self.assertEqual(result1['dominant_emotion'],'joy')
 
         result2=emotion_detection.emotion_detector("I am really mad about this.")
-        self.assertEqual(result2['emotion'], 'anger')
+        self.assertEqual(result2['dominant_emotion'], 'anger')
 
         result3=emotion_detection.emotion_detector("I feel disgusted just hearing about this")
-        self.assertEqual(result3['emotion'], 'disgust')
+        self.assertEqual(result3['dominant_emotion'], 'disgust')
 
         result4=emotion_detection.emotion_detector("I am so sad about this")
-        self.assertEqual(result4['emotion'], 'sadness')
+        self.assertEqual(result4['dominant_emotion'], 'sadness')
 
         result5=emotion_detection.emotion_detector("I am really afraid that this will happen")
-        self.assertEqual(result5['emotion'], 'fear')
+        self.assertEqual(result5['dominant_emotion'], 'fear')
+
+    unittest.main()
